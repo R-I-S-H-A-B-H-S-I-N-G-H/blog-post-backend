76cfb9765da3bd4da3797c9634309387c5f3608f
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2022-09-14 00:05:10 +0530

    updating the node version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 2001;
+const PORT = process.env.PORT || 3001;
 const blogs = require("./Routes/Blogs");
 const mongoose = require("mongoose");
 const BLOG = require("./models/blogsSchema");
